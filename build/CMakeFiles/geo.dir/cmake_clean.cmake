34886da1ab3fc7986e3fbd559e2a8464a08c7465
emirhanbayar <bayaremirhan07@gmail.com>
2022-09-14 17:19:19 +0300

    Simple layout of a ompl package

---
+++ build/CMakeFiles/geo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geo.dir/geometric_planning.cpp.o"
+  "geo"
+  "geo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/geo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
